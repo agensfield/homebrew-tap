907f0d79b8e76fde0728904064e15e70793241e6
Arda Sevinç <arda@ardasevinc.com>
2026-06-29 02:40:54 +0300

    scriba: update formula for v0.2.3

--- Formula/scriba.rb
+++ Formula/scriba.rb
@@ -1,25 +1,25 @@
 class Scriba < Formula
   desc "Local-first usage tracking for Claude Code and Codex"
   homepage "https://github.com/agensfield/scriba"
-  version "0.2.2"
+  version "0.2.3"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/agensfield/scriba/releases/download/v#{version}/scriba_#{version}_darwin_arm64.tar.gz"
-      sha256 "9de3429538d373301b142c3c9364409c8bb361dd354218c2af92bce9090c685d"
+      sha256 "bbfd76b55845974be69d4a8a43baa67c4386e75a862fcdf154ce92f354bc3591"
     else
       url "https://github.com/agensfield/scriba/releases/download/v#{version}/scriba_#{version}_darwin_amd64.tar.gz"
-      sha256 "9a432d1d834603e5c40812934b0215f81da0ce806b9b51c761c0143fee0928dc"
+      sha256 "f599f60b5813ad2f3980bb498abcf578ee991df7c42b24e24e6ef85e1bd97e7c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/agensfield/scriba/releases/download/v#{version}/scriba_#{version}_linux_arm64.tar.gz"
-      sha256 "d7d158110827ded93460dad7822871e7ea8a1e5ad256206667efd056542353a7"
+      sha256 "b41775a3797312c422bde2542a2054d7031f7fda9da2f4da77598d9a6c21106d"
     else
       url "https://github.com/agensfield/scriba/releases/download/v#{version}/scriba_#{version}_linux_amd64.tar.gz"
-      sha256 "d415b45190784d952ec9f051a49d15288014bb5efe121def37db61a72c0ee46d"
+      sha256 "e0a824c8f968bab7f342d1d630c419f2b39801c65d2e6e318dd4757057717d0f"
     end
   end
 
