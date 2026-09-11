15692cfbcbff2bdd91b0111807e6b0ee0b834d77
Arda Sevinç <arda@ardasevinc.com>
2026-09-11 13:31:51 +0000

    fix: update Verso formula to alpha.3

--- Formula/verso.rb
+++ Formula/verso.rb
@@ -1,26 +1,26 @@
 class Verso < Formula
   desc "Deliberate Codex account switching with one native working home"
   homepage "https://github.com/agensfield/verso"
-  version "0.1.0-alpha.2"
+  version "0.1.0-alpha.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.2/verso_0.1.0-alpha.2_darwin_arm64.tar.gz"
-      sha256 "3027e5f1a3a4ab88ba2e2ced3f42abbf6d342247d68dcb0473dd436315b125bf"
+      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.3/verso_0.1.0-alpha.3_darwin_arm64.tar.gz"
+      sha256 "340543412b07b874a21f3a61748cfd83ca777b7bc68668d97c1ac893f0d7e1a2"
     else
-      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.2/verso_0.1.0-alpha.2_darwin_amd64.tar.gz"
-      sha256 "c77e0856e1f50f370dfa0b3073060799b6a99ffac86f5aa845f22438a3202d6e"
+      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.3/verso_0.1.0-alpha.3_darwin_amd64.tar.gz"
+      sha256 "c854169b708a6c0362511d3464dd609788a532a5148310b5a31fc188fee86cc5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.2/verso_0.1.0-alpha.2_linux_arm64.tar.gz"
-      sha256 "ce7a904745668b0bd803099bf8f9092ec1406fc7d638d0bc2b18613aa4fd37a4"
+      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.3/verso_0.1.0-alpha.3_linux_arm64.tar.gz"
+      sha256 "49628fe758df4b19b02a1324b669328ad4d28864ee59514909df535a129cd494"
     else
-      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.2/verso_0.1.0-alpha.2_linux_amd64.tar.gz"
-      sha256 "cec26136762686db476fe08b6f84ef18de0eb3a12ed3ed5414c564ebcda7ca27"
+      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.3/verso_0.1.0-alpha.3_linux_amd64.tar.gz"
+      sha256 "d2e2a8f9ca8745b2993da6bbe91579d02e646fb225c938ca381a93dee5a11751"
     end
   end
 
