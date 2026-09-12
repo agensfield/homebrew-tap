ebb461ff8df434873ea3bb68d51abb3b4f2a53f2
Arda Sevinç <arda@ardasevinc.com>
2026-09-12 09:09:27 +0000

    chore: update verso to v0.1.0-alpha.5

--- Formula/verso.rb
+++ Formula/verso.rb
@@ -1,26 +1,26 @@
 class Verso < Formula
   desc "Deliberate Codex account switching with one native working home"
   homepage "https://github.com/agensfield/verso"
-  version "0.1.0-alpha.4"
+  version "0.1.0-alpha.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.4/verso_0.1.0-alpha.4_darwin_arm64.tar.gz"
-      sha256 "80f5980e75f9695afcb08cd023bd84bc58952898f208d0531363e5033fffac39"
+      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.5/verso_0.1.0-alpha.5_darwin_arm64.tar.gz"
+      sha256 "ab275ba38b56eecc0832f0c89bf2797ba6fbc958b295ab436d866b444c8fdf70"
     else
-      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.4/verso_0.1.0-alpha.4_darwin_amd64.tar.gz"
-      sha256 "911514bc868f7894de22e6f946daca069ab7195066268811a2b65be96cc11a48"
+      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.5/verso_0.1.0-alpha.5_darwin_amd64.tar.gz"
+      sha256 "9bbf38201b5857dcaec0ea15d3a6b76eb31b7cefb4b7a4a032d5da8954b6b8a5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.4/verso_0.1.0-alpha.4_linux_arm64.tar.gz"
-      sha256 "cd9c51933d2767f5d3c4258170159e16778efe8d3e091650f7a361b49f5b7df5"
+      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.5/verso_0.1.0-alpha.5_linux_arm64.tar.gz"
+      sha256 "576165c8f5811a411ebb28314ce90b3e0fdcc59450775b3600c9b07913d5206d"
     else
-      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.4/verso_0.1.0-alpha.4_linux_amd64.tar.gz"
-      sha256 "d151861248f2dbcf18ccb1f158769e608e9a6582a9cdf200e4c9695d1c4792f1"
+      url "https://github.com/agensfield/verso/releases/download/v0.1.0-alpha.5/verso_0.1.0-alpha.5_linux_amd64.tar.gz"
+      sha256 "ecda4007dacad41dd609f1f6852bf6ad5ec32d548bfeb4c67b0bb7e5fbc43184"
     end
   end
 
