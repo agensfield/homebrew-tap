228099c13e46fca2be71a675987cd4f53f05e979
Arda Sevinç <arda@ardasevinc.com>
2026-09-15 13:21:30 +0300

    style: use literal Ramiz release URLs

--- Formula/ramiz.rb
+++ Formula/ramiz.rb
@@ -1,27 +1,26 @@
 class Ramiz < Formula
   desc "Ridiculously cheap Git worktrees using filesystem copy-on-write"
   homepage "https://github.com/agensfield/ramiz"
-  version "1.0.1"
   license "MIT"
 
   depends_on "git"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/agensfield/ramiz/releases/download/v#{version}/ramiz_#{version}_darwin_arm64.tar.gz"
+      url "https://github.com/agensfield/ramiz/releases/download/v1.0.1/ramiz_1.0.1_darwin_arm64.tar.gz"
       sha256 "2122673bbbfdb0be4f90889e5360c439103879e886ddaa9f69addfd27ee8de5b"
     else
-      url "https://github.com/agensfield/ramiz/releases/download/v#{version}/ramiz_#{version}_darwin_amd64.tar.gz"
+      url "https://github.com/agensfield/ramiz/releases/download/v1.0.1/ramiz_1.0.1_darwin_amd64.tar.gz"
       sha256 "44b512f756ea19a1645fdbbae057a9a5e6d98a69af09885469eeb8320b5b370f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/agensfield/ramiz/releases/download/v#{version}/ramiz_#{version}_linux_arm64.tar.gz"
+      url "https://github.com/agensfield/ramiz/releases/download/v1.0.1/ramiz_1.0.1_linux_arm64.tar.gz"
       sha256 "ac3363f6466221c7673da5d4488168ffed47159ff9b17b018a00b35dcdd780c7"
     else
-      url "https://github.com/agensfield/ramiz/releases/download/v#{version}/ramiz_#{version}_linux_amd64.tar.gz"
+      url "https://github.com/agensfield/ramiz/releases/download/v1.0.1/ramiz_1.0.1_linux_amd64.tar.gz"
       sha256 "647f9ba32ff1b882e62b05eee8da7e21863f53d3cc9d2cfc20483c326cb44198"
     end
   end
